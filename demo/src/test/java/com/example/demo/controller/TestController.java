86693d3661f30685d83c60b39c6e74da205630db
newctl <newctl@qq.com>
2019-08-15 19:33:14 +0800

    initial

--- demo/src/test/java/com/example/demo/controller/TestController.java
+++ demo/src/test/java/com/example/demo/controller/TestController.java
@@ -1,4 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
+    @ResponseBody
+    public String hello(){
+        return "good morning";
+    }
 }
